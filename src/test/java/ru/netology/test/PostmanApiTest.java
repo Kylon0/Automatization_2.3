130022c57333a4a6d72e6c33a0e35274d01cf4fe
Kylon0 <wladikylon@gmail.com>
2022-02-21 03:14:50 +0300

    Initial commit 6

--- src/test/java/ru/netology/test/PostmanApiTest.java
+++ src/test/java/ru/netology/test/PostmanApiTest.java
@@ -15,7 +15,7 @@ public class PostmanApiTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("host", equalTo("postman-echo.com"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
