a1357a30310f0ed0827215c647652d779c469daf
Kylon0 <wladikylon@gmail.com>
2022-02-21 03:11:38 +0300

    Initial commit 3

--- src/test/java/ru/netology/test/PostmanApiTest.java
+++ src/test/java/ru/netology/test/PostmanApiTest.java
@@ -15,7 +15,7 @@ public class PostmanApiTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("so data"))
         ;
     }
 }
